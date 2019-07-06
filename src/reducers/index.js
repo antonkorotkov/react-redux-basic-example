499b5161fe95ac7f287cefda3cdc43f9889c52e3
Anton <antonkorotkoff@gmail.com>
2019-07-06 13:11:23 +0300

    complete

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+import { combineReducers } from 'redux'
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Song 2', duration: '2:44' },
+    { title: 'Song 3', duration: '1:55' },
+    { title: 'Song 4', duration: '2:55' }
+  ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if ( action.type === 'SONG_SELECTED' ) {
+    return action.payload;
+  }
+
+  return selectedSong;
+}
+
+export default combineReducers({ songs: songsReducer, selectedSong: selectedSongReducer })
